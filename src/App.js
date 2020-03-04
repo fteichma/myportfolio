94c1292fbc2c697674b88447e2ffbd45b0f0c864
Fabian Teichmann <fabiteich01@gmail.com>
2020-03-04 17:10:39 +0100

    Routing, etc..

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Hello, I'm</h2>
+          <h1>Fabian Teichmann</h1>
+          <div className="Description">
+            <p>I’m a <strong>frontend web & mobile developer.</strong></p>
+            <p>I am also a <strong>true design lover.</strong></p>
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import logo from './logo.svg';
 import './index.css';
+import { Route, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
+import Contact from './Contact';
+import NotFound from './NotFound';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const router = (
+    <Router>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink exact activeClassName="active" to="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/contact" component={Contact} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+)
+ReactDOM.render(router, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
